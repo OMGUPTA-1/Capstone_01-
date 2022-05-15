7e832a770e7c8074c22ab593aef0358b8d4025e5
OMGUPTA-1 <95625619+OMGUPTA-1@users.noreply.github.com>
2022-05-15 13:08:32 +0530

    Update SOH.py

--- soh/SOH.py
+++ soh/SOH.py
@@ -7,9 +7,17 @@ data = pd.read_csv('data_csv.csv')
 delta_v = data['Delta V']
 cycle = data['Cycle Count']
 delta_t = data['Delta T']
-
+# A and B are the data regression coefficients 
 A = 0.5192
 B = 0.0405
 soh = [(A * (delta_t[i]/delta_v[i]) + B)for i in range(len(cycle))] 
 plt.plot(cycle+1, soh)
+plt.ylabel('State of Health (%)')
+plt.xlabel('Number of cycles (N) ')
+plt.title(
+            "Battery State of Charge (SoH) vs\nNumber of cycles (N)", fontweight='bold')
+plt.grid(True)
+    
+
 plt.show()
+
